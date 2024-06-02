c5d9f92de336522941c66ad4761a5ce40e2fc72a
DreamMaoMao <2523610504@qq.com>
2024-06-02 10:07:52 +0800

    remove useless code

--- init.lua
+++ init.lua
@@ -5,8 +5,8 @@ local KEYS_LABLE = {
 
 local INPUT_KEY = {
 	"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
-	"o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y","z","0","1","2"
-	,"3","4","5","6","7","8","9","-","_",".","<Esc>"
+	"o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2"
+, "3", "4", "5", "6", "7", "8", "9", "-", "_", ".", "<Esc>"
 }
 
 local INPUT_CANDS = {
@@ -17,16 +17,16 @@ local INPUT_CANDS = {
 	{ on = "u" }, { on = "v" }, { on = "w" }, { on = "x" }, { on = "y" },
 	{ on = "z" }, { on = "0" }, { on = "1" }, { on = "2" }, { on = "3" },
 	{ on = "4" }, { on = "5" }, { on = "6" }, { on = "7" }, { on = "8" },
-	{ on = "9" }, { on = "-" }, { on = "_" }, { on = "." }, { on = "<Esc>"}
+	{ on = "9" }, { on = "-" }, { on = "_" }, { on = "." }, { on = "<Esc>" }
 }
 
 
-local function get_match_position(name,find_str)
+local function get_match_position(name, find_str)
 	if find_str == "" or find_str == nil then
-		return nil,nil
+		return nil, nil
 	end
 
-	local startPos,endPos = {},{}
+	local startPos, endPos = {}, {}
 	local startp, endp
 
 	-- record all match start position and end position
@@ -37,19 +37,19 @@ local function get_match_position(name,find_str)
 		if not startp then
 			break
 		end
-		table.insert(startPos,startp)
-		table.insert(endPos,endp)
+		table.insert(startPos, startp)
+		table.insert(endPos, endp)
 	end
 
 	if #startPos > 0 then
 		return startPos, endPos
 	else
-		return nil,nil
+		return nil, nil
 	end
 end
 
 -- apply search result to show
-local set_match_lable = ya.sync(function(state,url,name,file)
+local set_match_lable = ya.sync(function(state, url, name, file)
 	local span = {}
 	local key = {}
 	local i = 1
@@ -60,33 +60,34 @@ local set_match_lable = ya.sync(function(state,url,name,file)
 	local startPos = state.match[url].startPos
 	local endPos = state.match[url].endPos
 
-	table.insert(span,ui.Span(name:sub(1,startPos[1]-1)):fg(state.opt_unmatch_fg))
+	table.insert(span, ui.Span(name:sub(1, startPos[1] - 1)):fg(state.opt_unmatch_fg))
 	while i <= #startPos do
-		table.insert(span,ui.Span(name:sub(startPos[i],endPos[i])):fg(state.opt_match_str_fg):bg(state.opt_match_str_bg))
+		table.insert(span, ui.Span(name:sub(startPos[i], endPos[i])):fg(state.opt_match_str_fg):bg(state
+		.opt_match_str_bg))
 		if i <= #key then
-			table.insert(span,ui.Span(key[i]):fg(state.opt_lable_fg):bg(state.opt_lable_bg))
+			table.insert(span, ui.Span(key[i]):fg(state.opt_lable_fg):bg(state.opt_lable_bg))
 		end
 		if i + 1 <= #startPos then
 			if file:is_hovered() then
-				table.insert(span,ui.Span(name:sub(endPos[i]+1,startPos[i+1]-1)))
+				table.insert(span, ui.Span(name:sub(endPos[i] + 1, startPos[i + 1] - 1)))
 			else
-				table.insert(span,ui.Span(name:sub(endPos[i]+1,startPos[i+1]-1)):fg(state.opt_unmatch_fg))
+				table.insert(span, ui.Span(name:sub(endPos[i] + 1, startPos[i + 1] - 1)):fg(state.opt_unmatch_fg))
 			end
 		end
 		i = i + 1
 	end
-	
+
 	if file:is_hovered() then
-		table.insert(span,ui.Span(name:sub(endPos[i-1]+1,#name)))
+		table.insert(span, ui.Span(name:sub(endPos[i - 1] + 1, #name)))
 	else
-		table.insert(span,ui.Span(name:sub(endPos[i-1]+1,#name)):fg(state.opt_unmatch_fg))
+		table.insert(span, ui.Span(name:sub(endPos[i - 1] + 1, #name)):fg(state.opt_unmatch_fg))
 	end
-	
+
 	return span
 end)
 
 -- update the match data after input a str
-local update_match_table = ya.sync(function (state,folder,find_str)
+local update_match_table = ya.sync(function(state, folder, find_str)
 	if not folder then
 		return
 	end
@@ -96,7 +97,7 @@ local update_match_table = ya.sync(function (state,folder,find_str)
 	for _, file in ipairs(folder.window) do
 		local name = file.name:gsub("\r", "?", 1)
 		local url = tostring(file.url)
-		local startPos, endPos = get_match_position(name,find_str)
+		local startPos, endPos = get_match_position(name, find_str)
 		if startPos then
 			state.match[url] = {
 				key = {},
@@ -104,15 +105,15 @@ local update_match_table = ya.sync(function (state,folder,find_str)
 				endPos = endPos,
 			}
 			i = 1
-			while i <=  #startPos do
-				state.next_char[#state.next_char +1] = string.lower(name:sub(endPos[i]+1,endPos[i]+1))
+			while i <= #startPos do -- the next char of match string can't be used as lable for supporing further search
+				state.next_char[#state.next_char + 1] = string.lower(name:sub(endPos[i] + 1, endPos[i] + 1))
 				i = i + 1
 			end
 		end
 	end
 end)
 
-local record_match_file = ya.sync(function (state,find_str)
+local record_match_file = ya.sync(function(state, find_str)
 	local exist_match = false
 
 	if state.match == nil then
@@ -124,15 +125,15 @@ local record_match_file = ya.sync(function (state,find_str)
 	end
 
 	-- record match file from current window
-	update_match_table(Folder:by_kind(Folder.CURRENT),find_str)
+	update_match_table(Folder:by_kind(Folder.CURRENT), find_str)
 
 	-- record match file from parent window
-	update_match_table(Folder:by_kind(Folder.PARENT),find_str)
+	update_match_table(Folder:by_kind(Folder.PARENT), find_str)
 
 	-- record match file from preview window
-	update_match_table(Folder:by_kind(Folder.PREVIEW),find_str)
+	update_match_table(Folder:by_kind(Folder.PREVIEW), find_str)
 
-	-- get valid key list
+	-- get valid key list (KEYS_LABLE but exclude state.next_char table)
 	local valid_lable = {}
 	for _, value in ipairs(KEYS_LABLE) do
 		local found = false
@@ -142,23 +143,23 @@ local record_match_file = ya.sync(function (state,find_str)
 				break
 			end
 		end
-	
+
 		if not found then
-			table.insert(valid_lable,value)
+			table.insert(valid_lable, value)
 		end
 	end
 
 	-- assign valid key to each match file
 	local i = 1
-	local j 
+	local j
 	for url, _ in pairs(state.match) do
 		exist_match = true
 		j = 1
-		while j <= #state.match[url].startPos do  -- some file may match multi position
-			table.insert(state.match[url].key,valid_lable[i])
+		while j <= #state.match[url].startPos do -- some file may match multi position
+			table.insert(state.match[url].key, valid_lable[i])
 			i = i + 1
 			j = j + 1
-		end			
+		end
 	end
 
 	-- flush page
@@ -169,7 +170,6 @@ local record_match_file = ya.sync(function (state,find_str)
 end)
 
 local toggle_ui = ya.sync(function(st)
-
 	if st.highlights or st.mode then
 		File.highlights, Status.mode, st.highlights, st.mode = st.highlights, st.mode, nil, nil
 		ya.manager_emit("peek", { force = true })
@@ -179,21 +179,17 @@ local toggle_ui = ya.sync(function(st)
 
 	st.highlights, st.mode = File.highlights, Status.mode
 
-	if st.target_str == nil then
-		st.target_str = ""
-	end
-
 	File.highlights = function(self, file)
 		local span = {}
 		local name = file.name:gsub("\r", "?", 1)
 		local url = tostring(file.url)
 
 		if st.match and st.match[url] then
-			span = set_match_lable(url,name,file)
+			span = set_match_lable(url, name, file)
 		elseif file:is_hovered() then
-			span = ui.Span(name)	
+			span = ui.Span(name)
 		else
-			span = ui.Span(name):fg(st.opt_unmatch_fg)		
+			span = ui.Span(name):fg(st.opt_unmatch_fg)
 		end
 
 		return span
@@ -211,9 +207,9 @@ local toggle_ui = ya.sync(function(st)
 end)
 
 
-local set_target_str =ya.sync(function(state,input_str)
+local set_target_str = ya.sync(function(state, input_str)
 	local is_match_key = false
-	local final_input_str = input_str:sub(#input_str,#input_str)
+	local final_input_str = input_str:sub(#input_str, #input_str)
 	local found = false
 	if state.match then
 		for url, _ in pairs(state.match) do
@@ -223,8 +219,8 @@ local set_target_str =ya.sync(function(state,input_str)
 					break
 				end
 			end
-			
-			if found then -- if the last str match the lable key, toggle jump action
+
+			if found then -- if the last str match is a lable key, not a search char, toggle jump action
 				ya.manager_emit(url:match("[/\\]$") and "cd" or "reveal", { url })
 				is_match_key = true
 				break
@@ -232,27 +228,27 @@ local set_target_str =ya.sync(function(state,input_str)
 		end
 	end
 
+	-- clears the previously calculated data when input change
 	state.match = nil
 	state.next_char = nil
-	state.target_str = input_str
 
-
+	-- calculate match data
 	local exist_match = record_match_file(input_str)
 
+	-- apply match data to render
 	ya.render()
+
 	if is_match_key or not exist_match then
-		return true
+		return true -- hit lable key or no match file
 	else
 		return false
 	end
 end)
 
-local clear_state_str =ya.sync(function(state)
-	state.target_str = ""
+local clear_state_str = ya.sync(function(state)
 	state.match = nil
 	state.next_char = nil
 	ya.render()
-
 end)
 
 local set_opts_default = ya.sync(function(state)
@@ -277,25 +273,24 @@ end)
 return {
 	setup = function(state, opts)
 		-- Save the user configuration to the plugin's state
-		if (opts ~= nil and opts.opt_unmatch_fg ~= nil ) then
-			state.opt_unmatch_fg  = opts.opt_unmatch_fg
+		if (opts ~= nil and opts.opt_unmatch_fg ~= nil) then
+			state.opt_unmatch_fg = opts.opt_unmatch_fg
 		end
-		if (opts ~= nil and opts.opt_match_str_fg ~= nil ) then
-			state.opt_match_str_fg  = opts.opt_match_str_fg
+		if (opts ~= nil and opts.opt_match_str_fg ~= nil) then
+			state.opt_match_str_fg = opts.opt_match_str_fg
 		end
-		if (opts ~= nil and opts.opt_match_str_bg ~= nil ) then
-			state.opt_match_str_bg  = opts.opt_match_str_bg
+		if (opts ~= nil and opts.opt_match_str_bg ~= nil) then
+			state.opt_match_str_bg = opts.opt_match_str_bg
 		end
-		if (opts ~= nil and opts.opt_lable_fg ~= nil ) then
-			state.opt_lable_fg  = opts.opt_lable_fg
+		if (opts ~= nil and opts.opt_lable_fg ~= nil) then
+			state.opt_lable_fg = opts.opt_lable_fg
 		end
-		if (opts ~= nil and opts.opt_lable_bg ~= nil ) then
-			state.opt_lable_bg  = opts.opt_lable_bg
+		if (opts ~= nil and opts.opt_lable_bg ~= nil) then
+			state.opt_lable_bg = opts.opt_lable_bg
 		end
 	end,
 
 	entry = function(_, args)
-
 		set_opts_default()
 
 		toggle_ui()
@@ -309,11 +304,11 @@ return {
 
 			if INPUT_KEY[cand] == "<Esc>" then
 				break
-			end		
+			end
 
 			input_str = input_str .. INPUT_KEY[cand]
 
-			local want_exit = set_target_str(input_str)				
+			local want_exit = set_target_str(input_str)
 			if want_exit then
 				break
 			end
@@ -322,6 +317,5 @@ return {
 
 		clear_state_str()
 		toggle_ui()
-
 	end
 }
